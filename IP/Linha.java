b00e856923021d0510c54edc4369993c82a466e5
laiscarraro <37347778+laiscarraro@users.noreply.github.com>
2018-06-10 22:41:13 -0300

    Update Linha.java

--- IP/Linha.java
+++ IP/Linha.java
@@ -1,18 +1,21 @@
 package onibus;
+import java.util.Random;
 
 public class Linha {
     
-    Passageiro passageiro = new Passageiro();
-    Veiculo onibus = new Veiculo();
-    Parada parada = new Parada();
+    //instâncias de objetos:
     
-    private int tamanho; //em km
+    Random n = new Random(); //sera usado para gerar numeros aleatorios
+    
+    //atributos:
+    
+    private int tamanho; //em metros: depende da distância entre as paradas e do numero de paradas
     private int paradas; //numero de paradas
-
-    public Linha() {
-        
-    }
-
+    ListaParada paradasDaLinha;
+    
+    //métodos:
+    
+    //getters e setters
     public int getTamanho() {
         return tamanho;
     }
@@ -27,6 +30,31 @@ public class Linha {
 
     public void setParadas(int paradas) {
         this.paradas = paradas;
+    }
+    
+    //mostra o status da linha
+    public void status() {
+        System.out.println("");
+        System.out.println("Esta linha tem " + getParadas() + " paradas.");
+        System.out.println("");
+    }
+    
+    //construtor da linha
+    public Linha(int numParadas) {
+        setParadas(numParadas);
+        paradasDaLinha = new ListaParada(getParadas());
+        setTamanho(paradasDaLinha.tamanho);
+    }
+    
+    public void imprimeLinha() {
+        System.out.println("");
+        System.out.println("Nova linha criada!");
+        status();
+        System.out.println("");
+        paradasDaLinha.imprimeLista();
+        System.out.println("");
+        System.out.println("Esta linha tem " + getTamanho() + "m.");
+        System.out.println("");
     }
     
 }
